8f4669a094e4d11805987b56a4a3ecd8d1814ff0
TBog <thetbog@gmail.com>
2023-11-29 20:33:37 +0200

    show source image on small preview image click

--- app/src/main/java/rocks/tbog/livewallpaperit/preview/ThumbnailAdapter.java
+++ app/src/main/java/rocks/tbog/livewallpaperit/preview/ThumbnailAdapter.java
@@ -2,8 +2,11 @@ package rocks.tbog.livewallpaperit.preview;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.util.ArrayMap;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,7 +18,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Set;
 import rocks.tbog.livewallpaperit.R;
 import rocks.tbog.livewallpaperit.RecycleAdapterBase;
@@ -23,7 +25,7 @@ import rocks.tbog.livewallpaperit.WorkAsync.AsyncUtils;
 import rocks.tbog.livewallpaperit.data.SubTopic;
 import rocks.tbog.livewallpaperit.utils.ViewUtils;
 
-public class ThumbnailAdapter extends RecycleAdapterBase<SubTopic.Image, ThumbnailAdapter.ThumbnailHolder> {
+public class ThumbnailAdapter extends RecycleAdapterBase<ThumbnailAdapter.Item, ThumbnailAdapter.ThumbnailHolder> {
     private static final String TAG = ThumbnailAdapter.class.getSimpleName();
     private final int mWidth;
     private final Set<String> mInvalidMediaIdSet;
@@ -32,39 +34,50 @@ public class ThumbnailAdapter extends RecycleAdapterBase<SubTopic.Image, Thumbna
         super(new ArrayList<>());
         mWidth = width;
         mInvalidMediaIdSet = invalidMediaIdSet;
-        HashMap<String, SubTopic.Image> map = new HashMap<>();
+        ArrayMap<String, Uri> links = new ArrayMap<>();
+        for (var image : topic.images) {
+            if (image.isObfuscated != showObfuscated) continue;
+            if (image.isSource) {
+                links.put(image.mediaId, Uri.parse(image.url));
+            }
+        }
+        ArrayMap<String, Item> map = new ArrayMap<>();
         int delta = Integer.MAX_VALUE;
         for (var image : topic.images) {
             if (image.isSource) continue;
             if (image.isObfuscated != showObfuscated) continue;
-            var mapImage = map.get(image.mediaId);
-            if (mapImage == null || delta > Math.abs(mapImage.width - image.width)) {
+            var item = map.get(image.mediaId);
+            if (item == null || delta > Math.abs(item.image.width - image.width)) {
                 delta = Math.abs(mWidth - image.width);
-                map.put(image.mediaId, image);
+                item = new Item(image, links.get(image.mediaId));
+                map.put(image.mediaId, item);
             }
         }
         mItemList.addAll(map.values());
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ThumbnailHolder holder, @NonNull SubTopic.Image image) {
+    public void onBindViewHolder(@NonNull ThumbnailHolder holder, @NonNull Item item) {
         holder.mImageView.setImageResource(R.drawable.ic_launcher_background);
-        holder.mInvalidView.setVisibility(mInvalidMediaIdSet.contains(image.mediaId) ? View.VISIBLE : View.GONE);
+        holder.mInvalidView.setVisibility(mInvalidMediaIdSet.contains(item.image.mediaId) ? View.VISIBLE : View.GONE);
 
-        setImageViewSize(holder.mImageView, image);
+        holder.mImageView.setOnClickListener(
+                v -> ViewUtils.launchIntent(v, new Intent(Intent.ACTION_VIEW).setData(item.link)));
+
+        setImageViewSize(holder.mImageView, item.image);
 
         Activity activity = ViewUtils.getActivity(holder.itemView);
         final Bitmap[] bitmapWrapper = new Bitmap[] {null};
-        if (activity instanceof ComponentActivity)
+        if (activity instanceof ComponentActivity) {
             AsyncUtils.runAsync(
                     ((ComponentActivity) activity).getLifecycle(),
                     task -> {
                         InputStream inputStream = null;
                         try {
-                            URL url = new URL(image.url);
+                            URL url = new URL(item.image.url);
                             inputStream = url.openConnection().getInputStream();
                         } catch (IOException e) {
-                            Log.e(TAG, "image " + image.mediaId + " failed to open connection", e);
+                            Log.e(TAG, "image " + item.image.mediaId + " failed to open connection", e);
                         }
                         if (inputStream == null) task.cancel();
                         bitmapWrapper[0] = BitmapFactory.decodeStream(inputStream);
@@ -74,6 +87,7 @@ public class ThumbnailAdapter extends RecycleAdapterBase<SubTopic.Image, Thumbna
                             holder.mImageView.setImageBitmap(bitmapWrapper[0]);
                         }
                     });
+        }
     }
 
     public void setImageViewSize(@NonNull ImageView view, @NonNull SubTopic.Image image) {
@@ -103,6 +117,18 @@ public class ThumbnailAdapter extends RecycleAdapterBase<SubTopic.Image, Thumbna
             super(itemView);
             mImageView = itemView.findViewById(android.R.id.icon);
             mInvalidView = itemView.findViewById(R.id.invalid);
+        }
+    }
+
+    public static class Item {
+        @NonNull
+        final SubTopic.Image image;
+
+        final Uri link;
+
+        public Item(@NonNull SubTopic.Image image, Uri uri) {
+            this.image = image;
+            this.link = uri;
         }
     }
 }
